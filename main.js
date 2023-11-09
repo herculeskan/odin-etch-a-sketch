59abe9e9438e8bd1fd3eab4fc11bee0fe03bb101
Carlos <carloselara034@gmail.com>
2023-11-09 10:11:21 -0500

    falta agregar el js, queda pendiente

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-document.addEventListener('DOMContentLoaded',function(){
-  console.log("hi")
-})
+// se necesitan 3 acciones
+// 1.cada vez que se pase el click por el cuadro .board tiene que dibujar algo  (event listener)
+// 2. con el boton que dice back tiene que borrar lo que se dibujo
+//3. con el boton que dice random tiene que cambiar el color del dibujo
+//4. con el boton que dice reset tiene que borrar todo
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
